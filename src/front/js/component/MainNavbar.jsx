656a6ca2e8faed2b5a7faa824c81263a49e7d2d7
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-09-15 20:12:22 +0000

    Se ha corregido error de navbar que no desaparecía al hacer click Se ha ajustado el efecto scroll de la home para que aparezcan antes las cajas Se ha añadido location a routes (antes estaba) En el login se ha corregido los botones para que esten en la misma linea en pantallas pequeñas

--- src/front/js/component/MainNavbar.jsx
+++ src/front/js/component/MainNavbar.jsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import { useState } from "react";
+import React, { useContext, useState } from "react";
 import logoweb from "../../img/LogoWeb.png";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -11,49 +10,53 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import '../../styles/MainNavbar.css';
 
-
 export const MainNavbar = () => {
     const { store, actions } = useContext(Context);
-    const { isLoggedIn, setIsLoggedIn } = useState(false);
+    const [expanded, setExpanded] = useState(false); // controla el estado del nav
     const navigate = useNavigate();
 
     const handleLogout = () => {
         actions.setIsLoged(false);
-        navigate("/"); // Redirigir a la página de inicio después de cerrar sesión
+        setExpanded(false); // cierra el nav al salir
+        navigate("/"); // y redirige a la home
     };
 
+    const handleToggle = () => setExpanded(!expanded); // interructor de abrir y cerrar el menu
+
+    const handleSelect = () => setExpanded(false); // cierra el menu al hacer clickç
+
     return (
-        <Navbar expand="lg" className="estilo-navbar fixed-top text-uppercase" data-bs-theme="light">
+        <Navbar expand="lg" className="estilo-navbar fixed-top text-uppercase" data-bs-theme="light" expanded={expanded}>
             <Container fluid>
-                <Navbar.Brand as={Link} to="/"> 
+                <Navbar.Brand as={Link} to="/" onClick={handleSelect}>
                     <img src={logoweb} alt="Logo" className="d-inline-block align-top" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/trainers">Trainers</Nav.Link>
-                        <Nav.Link as={Link} to="/bmr-calculator">BMR Calculator</Nav.Link>
+                    <Nav className="me-auto" onSelect={handleSelect}>
+                        <Nav.Link as={Link} to="/trainers" onClick={handleSelect}>Trainers</Nav.Link>
+                        <Nav.Link as={Link} to="/bmr-calculator" onClick={handleSelect}>BMR Calculator</Nav.Link>
                         <NavDropdown title="TrAIner" id="trainer-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/generate-recipes">Generar Receta</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/generate-routines">Generar Rutina</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/generate-recipes" onClick={handleSelect}>Generar Receta</NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/generate-routines" onClick={handleSelect}>Generar Rutina</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link as={Link} to="/exercises">Ejercicios</Nav.Link>
+                        <Nav.Link as={Link} to="/exercises" onClick={handleSelect}>Ejercicios</Nav.Link>
                     </Nav>
-                    <Nav className="ms-auto">
+                    <Nav className="ms-auto" onSelect={handleSelect}>
                         {store.isLoged ? (
                             <NavDropdown title={<i className="fa-solid fa-user"></i>} id="user-nav-dropdown" align="end">
-                                <NavDropdown.Item as={Link} to="/profile">Perfil</NavDropdown.Item> {/* Aquí cambia */}
+                                <NavDropdown.Item as={Link} to="/profile" onClick={handleSelect}>Perfil</NavDropdown.Item>
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item onClick={handleLogout}>Cerrar sesión</NavDropdown.Item>
                             </NavDropdown>
                         ) : (
-                            <>
-                                <Button variant="outline-dark mx-1" onClick={() => navigate("/login")}><i className="fa-solid fa-user-plus"></i></Button>
-                            </>
+                            <Button variant="outline-dark mx-1" onClick={() => { setExpanded(false); navigate("/login"); }}>
+                                <i className="fa-solid fa-user-plus"></i>
+                            </Button>
                         )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
