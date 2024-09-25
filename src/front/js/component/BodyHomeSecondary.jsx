3459fef9f347cbff7ced6190cc91acde966a0254
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-09-25 07:40:31 +0000

    [CARDS CARRUSEL HOME] Done

--- src/front/js/component/BodyHomeSecondary.jsx
+++ src/front/js/component/BodyHomeSecondary.jsx
@@ -6,6 +6,8 @@ import rejasHome from "../../img/home/body-home/rejas-para-ventanas-sencillas-y-
 import correderasHome from "../../img/home/body-home/puertas-correderas-automaticas-en-ciudad-real.avif";
 import valladosHome from "../../img/home/body-home/vallados-metalicos-exteriores-en-ciudad-real.avif";
 import peatonalesHome from "../../img/home/body-home/puertas-peatonales-en-ciudad-real.avif";
+import correderasInterioresHome from "../../img/home/body-home/puertas-correderas-interiores.avif";
+import cerramientoCocinaHome from "../../img/home/body-home/cerramiento-de-cocina-con-cristal.jpg";
 import { useNavigate } from "react-router-dom";
 
 export const BodyHomeSecondary = () => {
@@ -19,19 +21,16 @@ export const BodyHomeSecondary = () => {
             <div className="row">
                 <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 mt-2">
                     <Card className="text-white" style={{ height: '300px' }}>
-                        <div
-                            style={{
+                        <div style={{
                                 backgroundImage: `url(${rejasHome})`,
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
-                                height: '100%'
-                            }}
-                        >
+                                height: '100%'}}>
                             <Card.ImgOverlay className="m-3">
                                 <h2 className="h2-home">Rejas para Ventanas</h2>
                                 <p className="p-home">Diseño Moderno</p>
                                 <div className="my-5">
-                                    <Button className="btn-style-background-color">Explorar</Button>{' '}
+                                    <Button className="btn-style-background-color">Ir</Button>{' '}
                                 </div>
                             </Card.ImgOverlay>
                         </div>
@@ -39,19 +38,16 @@ export const BodyHomeSecondary = () => {
                 </div>
                 <div className="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 mt-2">
                     <Card className="text-white" style={{ height: '300px' }}>
-                        <div
-                            style={{
+                        <div style={{
                                 backgroundImage: `url(${correderasHome})`,
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
-                                height: '100%'
-                            }}
-                        >
+                                height: '100%'}}>
                             <Card.ImgOverlay className="m-3">
                                 <h2 className="h2-home">Correderas Exteriores</h2>
                                 <p className="p-home">Automáticas</p>
                                 <div className="my-5">
-                                    <Button className="btn-style-background-color">Explorar</Button>{' '}
+                                    <Button className="btn-style-background-color">Descubrir</Button>{' '}
                                 </div>
                             </Card.ImgOverlay>
                         </div>
@@ -59,19 +55,16 @@ export const BodyHomeSecondary = () => {
                 </div>
                 <div className="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 mt-2">
                     <Card className="text-white" style={{ height: '300px' }}>
-                        <div
-                            style={{
+                        <div style={{
                                 backgroundImage: `url(${valladosHome})`,
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
-                                height: '100%'
-                            }}
-                        >
+                                height: '100%'}}>
                             <Card.ImgOverlay className="m-3">
                                 <h2 className="h2-home">Vallados Metálicos</h2>
                                 <p className="p-home">Diseño innovador</p>
                                 <div className="my-5">
-                                    <Button className="btn-style-background-color">Explorar</Button>{' '}
+                                    <Button className="btn-style-background-color">Más</Button>{' '}
                                 </div>
                             </Card.ImgOverlay>
                         </div>
@@ -79,19 +72,50 @@ export const BodyHomeSecondary = () => {
                 </div>
                 <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 mt-2">
                     <Card className="text-white" style={{ height: '300px' }}>
-                        <div
-                            style={{
+                        <div style={{
                                 backgroundImage: `url(${peatonalesHome})`,
                                 backgroundSize: 'cover',
                                 backgroundPosition: 'center',
-                                height: '100%'
-                            }}
-                        >
+                                height: '100%'}}>
                             <Card.ImgOverlay className="m-3">
                                 <h2 className="h2-home">Puertas Peatonales</h2>
                                 <p className="p-home">A la Vanguardia</p>
                                 <div className="my-5">
+                                    <Button className="btn-style-background-color">Ver</Button>{' '}
+                                </div>
+                            </Card.ImgOverlay>
+                        </div>
+                    </Card>
+                </div>
+                <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 mt-2">
+                    <Card className="text-white" style={{ height: '300px' }}>
+                        <div style={{
+                                backgroundImage: `url(${correderasInterioresHome})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                height: '100%'}}>
+                            <Card.ImgOverlay className="m-3">
+                                <h2 className="h2-home">Puertas Correderas Interiores</h2>
+                                <p className="p-home">Con estilo</p>
+                                <div className="my-5">
                                     <Button className="btn-style-background-color">Explorar</Button>{' '}
+                                </div>
+                            </Card.ImgOverlay>
+                        </div>
+                    </Card>
+                </div>
+                <div className="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 mt-2">
+                    <Card className="text-white" style={{ height: '300px' }}>
+                        <div style={{
+                                backgroundImage: `url(${cerramientoCocinaHome})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                height: '100%'}}>
+                            <Card.ImgOverlay className="m-3">
+                                <h2 className="h2-home">Cerramientos de Cocina</h2>
+                                <p className="p-home">Tendencias</p>
+                                <div className="my-5">
+                                    <Button className="btn-style-background-color">Investigar</Button>{' '}
                                 </div>
                             </Card.ImgOverlay>
                         </div>
--- src/front/js/component/CardsCarrusel.jsx
+++ src/front/js/component/CardsCarrusel.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import "../../styles/cards-carrusel.css";
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import albanyImage from "../../img/cards-carrusel/rejas-para-ventanas.avif";
+import Rating from 'react-rating'; 
+import "../../styles/home.css";
 
 export const CardsCarrusel = () => {
     const options = {
         margin: 10,
         responsive: {
-            0: {
-                items: 1
-            },
-            481: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            1199: {
-                items: 4
-            }
+            0: { items: 1 },
+            481: { items: 2 },
+            768: { items: 3 },
+            1199: { items: 4 }
         },
-        nav: false,  
-        dots: true,  
+        nav: false,
+        dots: true,
         loop: true
     };
 
@@ -31,76 +26,35 @@ export const CardsCarrusel = () => {
         <div className="container mb-5">
             <div className="row">
                 <OwlCarousel className='owl-theme' {...options}>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
+                    {[1, 2, 3, 4, 5, 6].map((item) => (
+                        <div className="item p-2 mb-5" key={item}>
+                            <div className="card-carrusel bg-light">
+                                <div className="card-body-carrusel">
+                                    <img src={albanyImage} alt="Rejas para ventanas" />
+                                </div>
+                                <div className="card-body-carrusel-info">
+                                    <h6 className="card-title">Rejas Albany</h6>
+                                    <p className="card-text-carrusel">
+                                        <span className="current-price">120€/m²</span>
+                                        <span className="discounted-price">150€/m2</span>
+                                        <span className="discount-percent">20% OFF</span>
+                                    </p>
+                                    <div className="rating">
+                                        <Rating
+                                            emptySymbol="fa fa-star-o"
+                                            fullSymbol="fa fa-star"
+                                            fractions={2}
+                                            initialRating={4} // Ajusta la puntuación inicial aquí
+                                            readonly // Pone el rating en modo solo lectura
+                                        />
+                                    </div>
+                                    <div className="my-1">
+                                        <Button className="btn-style-background-color">ver</Button>{' '}
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="card">
-                            <div className="card-body bg-light">
-                                <img src={albanyImage} alt="Rejas para ventanas" />
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </OwlCarousel>
             </div>
         </div>
