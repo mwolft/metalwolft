abaf4f568353f9b69a1cc71315ba3b7cb8060116
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-09-25 18:39:38 +0000

    Updating categories component

--- src/front/js/component/Product.jsx
+++ src/front/js/component/Product.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import "../../styles/cards-carrusel.css";
 import Button from 'react-bootstrap/Button';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Rating from 'react-rating';
 import Card from 'react-bootstrap/Card';
 import albanyImage from "../../img/cards-carrusel/rejas-para-ventanas.avif";
 
@@ -9,38 +13,302 @@ export const Product = () => {
     return (
         <div className="row my-5">
             <div className="col-12 col-md-6 col-lg-4 col-xl-4">
-                <Card className="px-2" style={{ width: 'auto'}}>
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
                     <Card.Img variant="top" src={albanyImage} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
             <div className="col-12 col-md-6 col-lg-4 col-xl-4">
-                <Card className="px-2" style={{ width: 'auto'}}>
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
                     <Card.Img variant="top" src={albanyImage} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
             <div className="col-12 col-md-6 col-lg-4 col-xl-4">
-                <Card className="px-2" style={{ width: 'auto'}}>
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
                     <Card.Img variant="top" src={albanyImage} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                <Card className="px-2 my-3" style={{ width: 'auto' }}>
+                    <Card.Img variant="top" src={albanyImage} />
+                    <Card.Body>
+                        <h6 className="card-title">Rejas Albany</h6>
+                        <p className="card-text-carrusel">
+                            <span className="current-price">120€/m²</span>
+                            <span className="discounted-price">150€/m2</span>
+                            <span className="discount-percent">20% OFF</span>
+                        </p>
+                        <div className="rating">
+                            <Rating
+                                emptySymbol="fa fa-star-o"
+                                fullSymbol="fa fa-star"
+                                fractions={2}
+                                initialRating={4} // Ajusta la puntuación inicial aquí
+                                readonly // Pone el rating en modo solo lectura
+                            />
+                        </div>
+                        <div className="my-1">
+                            <Button className="btn-style-background-color">Ver más</Button>{' '}
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
