32948fbb11a3f18559cfbbafa4a252e94db9457f
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-10-14 14:35:45 +0000

    Cart: styles setted

--- src/front/js/component/PayPalButton.jsx
+++ src/front/js/component/PayPalButton.jsx
@@ -3,27 +3,29 @@ import { PayPalButtons } from "@paypal/react-paypal-js";
 
 const PayPalButton = ({ amount, onSuccess }) => {
     return (
-        <PayPalButtons
-            style={{ layout: 'vertical' }}
-            createOrder={(data, actions) => {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: amount, // Monto total de la compra
-                        },
-                    }],
-                });
-            }}
-            onApprove={(data, actions) => {
-                return actions.order.capture().then((details) => {
-                    onSuccess(details); // Llama la función onSuccess tras una compra exitosa
-                });
-            }}
-            onError={(err) => {
-                console.error("PayPal Checkout onError", err);
-            }}
-        />
-    );
+        <div style={{ minHeight: '200px', overflow: 'hidden', marginBottom: '20px' }}> {/* Ajusta según el tamaño necesario */}
+            <PayPalButtons
+                style={{ layout: 'vertical' }}
+                createOrder={(data, actions) => {
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: amount, // Monto total de la compra
+                            },
+                        }],
+                    });
+                }}
+                onApprove={(data, actions) => {
+                    return actions.order.capture().then((details) => {
+                        onSuccess(details); // Llama la función onSuccess tras una compra exitosa
+                    });
+                }}
+                onError={(err) => {
+                    console.error("PayPal Checkout onError", err);
+                }}
+            />
+        </div>
+    );    
 }
 
 export default PayPalButton;
