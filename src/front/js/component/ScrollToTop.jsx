4a6ea5c10240b7f359842d9a6b29b719f5643cdc
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-11-15 05:33:11 +0000

    position: fixed

--- src/front/js/component/ScrollToTop.jsx
+++ src/front/js/component/ScrollToTop.jsx
@@ -1,14 +1,19 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
+
     useEffect(() => {
-        window.scrollTo(0, 0);
+        const mainContent = document.getElementById("main-content");
+        if (mainContent) {
+            mainContent.scrollIntoView({ behavior: 'auto', block: 'start' });
+        } else {
+            window.scrollTo(0, 0);
+        }
     }, [pathname]);
-	    return null; 
+
+    return null;
 };
 
 export default ScrollToTop;
-
