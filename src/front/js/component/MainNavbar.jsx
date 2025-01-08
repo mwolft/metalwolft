44548ff9e305283daffc8ce105901c0bdec9ea3b
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2025-01-08 11:40:35 +0000

    Banner navbar

--- src/front/js/component/MainNavbar.jsx
+++ src/front/js/component/MainNavbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import logoweb from "../../img/herrero-soldador-en-ciudad-real.png";
+import logoweb from "../../img/logo.png";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -15,9 +15,9 @@ export const MainNavbar = () => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        actions.setIsLoged(false); 
+        actions.setIsLoged(false);
         setExpanded(false);
-        navigate("/"); 
+        navigate("/");
     };
 
     const handleToggle = () => setExpanded(!expanded);
@@ -32,101 +32,106 @@ export const MainNavbar = () => {
     };
 
     return (
-        <Navbar expand="lg" className="estilo-navbar fixed-top text-uppercase" data-bs-theme="light" expanded={expanded}>
-            <Container fluid>
-                <Navbar.Brand as={Link} to="/" onClick={handleSelect}>
-                    <img src={logoweb} alt="Logo" className="d-inline-block align-top" />
-                </Navbar.Brand>
-                <div className="d-flex align-items-center justify-content-center">
-                    <Nav.Link onClick={handleFavoritesClick} className="d-flex align-items-center position-relative d-lg-none">
-                        <i className="fa-regular fa-heart fa-lg"></i>
-                        {store.isLoged && (
-                            <span className="position-absolute badge rounded-pill bg-danger favorites-badge">
-                                {store.favorites.length}
-                                <span className="visually-hidden">favoritos</span>
-                            </span>
-                        )}
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/cart" className="d-flex align-items-center position-relative d-lg-none">
-                        <i className="fa-solid fa-cart-shopping fa-lg"></i>
-                        {store.isLoged && (
-                            <span className="position-absolute badge rounded-pill bg-danger favorites-badge">
-                                {store.cart.length}
-                                <span className="visually-hidden">productos en el carrito</span>
-                            </span>
-                        )}
-                    </Nav.Link>
-                    {store.isLoged ? (
-                        <Nav.Link onClick={handleLogout} className="d-flex align-items-center d-lg-none">
-                            <i className="fa-solid fa-right-to-bracket fa-lg me-2"></i>
-                            <p className="small mb-0"></p>
-                        </Nav.Link>
-                    ) : (
-                        <Nav.Link onClick={() => { setExpanded(false); navigate("/login"); }} className="d-flex align-items-center d-lg-none">
-                            <i className="fa-regular fa-user fa-lg me-2"></i>
-                            <p className="small mb-0"></p>
-                        </Nav.Link>
-                    )}
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-                </div>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto" onSelect={handleSelect}>
-                        <Nav.Link as={Link} to="/" onClick={handleSelect}>Inicio</Nav.Link>
-                        <NavDropdown title="Productos" id="trainer-nav-dropdown">
-                            <NavDropdown.Item as={Link} to="/rejas-para-ventanas" onClick={handleSelect}>Rejas para Ventanas</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/vallados-metalicos-exteriores" onClick={handleSelect}>Vallados Metálicos Exteriores</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/puertas-peatonales-metalicas" onClick={handleSelect}>Puertas Peatonales Metálicas</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/puertas-correderas-exteriores" onClick={handleSelect}>Puertas Correderas Exteriores</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/cerramientos-de-cocina-con-cristal" onClick={handleSelect}>Cerramientos de Cocina con Cristal</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/puertas-correderas-interiores" onClick={handleSelect}>Puertas Correderas Interiores</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link as={Link} to="/blogs" onClick={handleSelect}>Blog</Nav.Link>
-                        {store.isLoged && store.currentUser?.is_admin && (
-                            <Nav.Link as={Link} to="/admin" onClick={handleSelect} className="d-lg-none" style={{marginLeft: '1.3rem', border: 'solid 2px #ff324d', borderRadius: '10px'}}>
-                               <i className="fa-solid fa-toolbox"></i> Admin
-                            </Nav.Link>
-                        )}
-                    </Nav>
-                    <Nav className="ms-auto d-none d-lg-flex" onSelect={handleSelect}>
-                        <Nav.Link onClick={handleFavoritesClick} className="d-flex align-items-center position-relative">
+        <>
+            <div className="top-banner text-center py-2">
+                <i className="fa-solid fa-truck-fast"></i> Envíos Gratis - Pedidos mínimos de 150€ 
+            </div>
+            <Navbar expand="lg" className="estilo-navbar fixed-top text-uppercase" data-bs-theme="light" expanded={expanded}>
+                <Container fluid>
+                    <Navbar.Brand as={Link} to="/" onClick={handleSelect}>
+                        <img src={logoweb} alt="Logo" className="d-inline-block align-top" />
+                    </Navbar.Brand>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <Nav.Link onClick={handleFavoritesClick} className="d-flex align-items-center position-relative d-lg-none">
                             <i className="fa-regular fa-heart fa-lg"></i>
                             {store.isLoged && (
-                                <span className="position-absolute badge rounded-pill favorites-badge">
+                                <span className="position-absolute badge rounded-pill bg-danger favorites-badge">
                                     {store.favorites.length}
                                     <span className="visually-hidden">favoritos</span>
                                 </span>
                             )}
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/cart" className="d-flex align-items-center position-relative">
+                        <Nav.Link as={Link} to="/cart" className="d-flex align-items-center position-relative d-lg-none">
                             <i className="fa-solid fa-cart-shopping fa-lg"></i>
                             {store.isLoged && (
-                                <span className="position-absolute badge rounded-pill favorites-badge">
+                                <span className="position-absolute badge rounded-pill bg-danger favorites-badge">
                                     {store.cart.length}
                                     <span className="visually-hidden">productos en el carrito</span>
                                 </span>
                             )}
                         </Nav.Link>
                         {store.isLoged ? (
-                            <>
-                                {store.currentUser?.is_admin && (
-                                    <Nav.Link as={Link} to="/admin" onClick={handleSelect} style={{marginLeft: '1.3rem', border: 'solid 2px #ff324d', borderRadius: '10px'}}>
-                                       <i className="fa-solid fa-toolbox"></i> Admin
-                                    </Nav.Link>
-                                )}
-                                <Nav.Link onClick={handleLogout} className="button-navbar d-flex align-items-center">
-                                    <i className="fa-solid fa-right-to-bracket fa-lg me-2"></i>
-                                    <p className="small mb-0">Salir</p>
-                                </Nav.Link>
-                            </>
+                            <Nav.Link onClick={handleLogout} className="d-flex align-items-center d-lg-none">
+                                <i className="fa-solid fa-right-to-bracket fa-lg me-2"></i>
+                                <p className="small mb-0"></p>
+                            </Nav.Link>
                         ) : (
-                            <Nav.Link onClick={() => { setExpanded(false); navigate("/login"); }} className="button-navbar d-flex align-items-center">
+                            <Nav.Link onClick={() => { setExpanded(false); navigate("/login"); }} className="d-flex align-items-center d-lg-none">
                                 <i className="fa-regular fa-user fa-lg me-2"></i>
-                                <p className="small mb-0">Iniciar sesión</p>
+                                <p className="small mb-0"></p>
                             </Nav.Link>
                         )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
+                    </div>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="m-auto" onSelect={handleSelect}>
+                            <Nav.Link as={Link} to="/" onClick={handleSelect}>Inicio</Nav.Link>
+                            <NavDropdown title="Productos" id="trainer-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/rejas-para-ventanas" onClick={handleSelect}>Rejas para Ventanas</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/vallados-metalicos-exteriores" onClick={handleSelect}>Vallados Metálicos Exteriores</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/puertas-peatonales-metalicas" onClick={handleSelect}>Puertas Peatonales Metálicas</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/puertas-correderas-exteriores" onClick={handleSelect}>Puertas Correderas Exteriores</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/cerramientos-de-cocina-con-cristal" onClick={handleSelect}>Cerramientos de Cocina con Cristal</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/puertas-correderas-interiores" onClick={handleSelect}>Puertas Correderas Interiores</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link as={Link} to="/blogs" onClick={handleSelect}>Blog</Nav.Link>
+                            {store.isLoged && store.currentUser?.is_admin && (
+                                <Nav.Link as={Link} to="/admin" onClick={handleSelect} className="d-lg-none" style={{ marginLeft: '1.3rem', border: 'solid 2px #ff324d', borderRadius: '10px' }}>
+                                    <i className="fa-solid fa-toolbox"></i> Admin
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                        <Nav className="ms-auto d-none d-lg-flex" onSelect={handleSelect}>
+                            <Nav.Link onClick={handleFavoritesClick} className="d-flex align-items-center position-relative">
+                                <i className="fa-regular fa-heart fa-lg"></i>
+                                {store.isLoged && (
+                                    <span className="position-absolute badge rounded-pill favorites-badge">
+                                        {store.favorites.length}
+                                        <span className="visually-hidden">favoritos</span>
+                                    </span>
+                                )}
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/cart" className="d-flex align-items-center position-relative">
+                                <i className="fa-solid fa-cart-shopping fa-lg"></i>
+                                {store.isLoged && (
+                                    <span className="position-absolute badge rounded-pill favorites-badge">
+                                        {store.cart.length}
+                                        <span className="visually-hidden">productos en el carrito</span>
+                                    </span>
+                                )}
+                            </Nav.Link>
+                            {store.isLoged ? (
+                                <>
+                                    {store.currentUser?.is_admin && (
+                                        <Nav.Link as={Link} to="/admin" onClick={handleSelect} style={{ marginLeft: '1.3rem', border: 'solid 2px #ff324d', borderRadius: '10px' }}>
+                                            <i className="fa-solid fa-toolbox"></i> Admin
+                                        </Nav.Link>
+                                    )}
+                                    <Nav.Link onClick={handleLogout} className="button-navbar d-flex align-items-center">
+                                        <i className="fa-solid fa-right-to-bracket fa-lg me-2"></i>
+                                        <p className="small mb-0">Salir</p>
+                                    </Nav.Link>
+                                </>
+                            ) : (
+                                <Nav.Link onClick={() => { setExpanded(false); navigate("/login"); }} className="button-navbar d-flex align-items-center">
+                                    <i className="fa-regular fa-user fa-lg me-2"></i>
+                                    <p className="small mb-0">Iniciar sesión</p>
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
